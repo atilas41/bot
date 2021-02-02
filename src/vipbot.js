ae4d39f17401e2ce8a95167f320dccb0969f74d8
atilas41 <78065247+atilas41@users.noreply.github.com>
2021-02-01 22:48:17 -0500

    Add files via upload

---
+++ src/vipbot.js
@@ -0,0 +1,15 @@
+const vipbot = (prefix) => {
+    return `💎 Acre-bot vip exclusivo💎
+🔰 *TABELA DE PREÇOS* 🔰
+
+🎉Ensinar a baixar e manusear o bot pelo termux: R$: 10,00 🎉
+
+🎉Criar um número exclusivo para o bot: R$: 7,00 🎉
+
+🎉 Outros comandos: negocie com o criador pra ver se é possivel colocar 🎉
+
+🔰 *MAIS INFORMAÇÕES* 🔰
+
+Dê: ${prefix}criador para negociar`
+}
+exports.vipbot = vipbot
